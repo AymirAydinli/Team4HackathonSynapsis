3c4314f3efa1483dcf866331dc5dfb3a77ee1715
Michal <wiewiorko.m@pg.com>
2023-01-28 09:39:09 +0100

    added button inside modal

--- syn-app-sis-nextjs/components/modal.tsx
+++ syn-app-sis-nextjs/components/modal.tsx
@@ -1,15 +1,34 @@
 import React from "react";
-
+import { useRouter } from 'next/navigation';
 
 const Modal = ({isVisible, score}) => {
+  const router = useRouter();
   if( !isVisible) return null;
   return(
+    <main>
+
     <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
       <div className="w-[600px]">
-        <div className="bg-white p-2 rounded">Dziękujemy za wypełnienie formularza. Wynik to {score}</div>
+        <div className="bg-white p-2 rounded">Dziękujemy za wypełnienie formularza. Wynik to {score}
+        <div className=" flex justify-center items-center p-2">
+        <button
+            onClick={() => router.push("/")}
+            type="button"
+            className="group relative flex-center justify-center
+            py-2 px-4 border border-transparent text-sm font-medium
+            rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+          >
+           Zakończ ankietę
+          </button>
+        </div>
+        </div>
 
       </div>
+      
     </div>
+
+    </main>
+    
   )
 
 }
